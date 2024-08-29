eac55d603c1e494e312ca7b6fb73a1466d14baa3
samim101 <samimreza2111@gmail.com>
2024-08-29 08:40:06 +0600

    remote page

--- carcontrol/server.js
+++ carcontrol/server.js
@@ -2,12 +2,27 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'car-run.html'));
 });
+
 app.get('/sensor', (req, res) => {
   res.sendFile(path.join(__dirname, 'sensor.html'));
 });
+app.get('/arm', (req, res) => {
+  res.sendFile(path.join(__dirname, './armcontrol/index.html'));
+});
+
+app.get('/:imageName', (req, res) => {
+  if (req.url.endsWith('.png') || req.url.endsWith('.jpeg'))
+  {
+    const fileName = req.url.split('/').pop();
+    return res.sendFile(path.join(__dirname, 'armcontrol', fileName));
+  }
+});
+
 
 const PORT = 3000;
 app.listen(PORT, () => {
